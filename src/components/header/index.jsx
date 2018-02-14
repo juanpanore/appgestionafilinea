9743f3b4ef7d89fed2d12d8b21d17f2e1181bd76
Cesar <cesarag89@gmail.com>
2018-02-14 16:33:54 -0500

    menu update

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,65 +8,70 @@ import PropTypes from "prop-types";
 import logo from "../../images/Logosura.png";
 
 class Header extends Component {
-    state = { open: false, text: "" };
+	state = { open: false, text: "" };
 
-    componentDidMount() {
-        axios.post(`http://localhost:8085/facturas/todas`).then(res => {
-            const text = res.data;
-            this.setState({ text });
-        });
-    }
+	componentDidMount() {
+		axios.post(`http://localhost:8085/facturas/todas`).then(res => {
+			const text = res.data;
+			this.setState({ text });
+		});
+	}
 
-    handleToggle = () => this.setState({ open: !this.state.open });
-    handleClose = () => this.setState({ open: false });
+	handleToggle = () => this.setState({ open: !this.state.open });
+	handleClose = () => this.setState({ open: false });
 
-    render() {
-        return (
-            <div>
-                <div>
-                    <img src={logo} alt="Sura" width="140dp" height="60dp" />
-                </div>
-                <AppBar
-                    title={this.props.title}
-                    iconClassNameRight="muidocs-icon-navigation-expand-more"
-                    onLeftIconButtonClick={this.handleToggle}
-                />
-                <Drawer docked={false} open={this.state.open} onRequestChange={open => this.setState({ open })}>
-                    <MenuItem onClick={this.handleClose} />
-                    <MenuItem onClick={this.handleClose}>
-                        <Link href="/" to="/">
-                            Inicio
-                        </Link>
-                    </MenuItem>
-                    <MenuItem onClick={this.handleClose}>
-                        <Link href="/facturasPendientes" to="/facturasPendientes">
-                            Facturas Pendientes
-                        </Link>
-                    </MenuItem>
-                    <MenuItem onClick={this.handleClose}>
-                        <Link href="/facturasAuditadas" to="/facturasAuditadas">
-                            Facturas Auditadas
-                        </Link>
-                    </MenuItem>
-                    <MenuItem onClick={this.handleClose}>
-                        <Link href="/facturasPagadas" to="/facturasPagadas">
-                            Facturas Pagadas
-                        </Link>
-                    </MenuItem>
-                    <MenuItem onClick={this.handleClose}>
-                        <Link href="/facturasDevueltas" to="/facturasDevueltas">
-                            Facturas Devueltas
-                        </Link>
-                    </MenuItem>
-                </Drawer>
-                <div>Respuesta de servicio: {this.state.text}</div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<div>
+					<img src={logo} alt="Sura" width="140dp" height="60dp" />
+				</div>
+				<AppBar
+					title={this.props.title}
+					iconClassNameRight="muidocs-icon-navigation-expand-more"
+					onLeftIconButtonClick={this.handleToggle}
+				/>
+				<Drawer docked={false} open={this.state.open} onRequestChange={open => this.setState({ open })}>
+					<MenuItem onClick={this.handleClose} />
+					<MenuItem onClick={this.handleClose}>
+						<Link href="/" to="/">
+							Inicio
+						</Link>
+					</MenuItem>
+					<MenuItem onClick={this.handleClose}>
+						<Link href="/facturasPendientes" to="/facturasPendientes">
+							Facturas Pendientes
+						</Link>
+					</MenuItem>
+					<MenuItem onClick={this.handleClose}>
+						<Link href="/facturasPendientes" to="/facturasPendientes">
+							Facturas Radicadas
+						</Link>
+					</MenuItem>
+					<MenuItem onClick={this.handleClose}>
+						<Link href="/facturasAuditadas" to="/facturasAuditadas">
+							Facturas Auditadas
+						</Link>
+					</MenuItem>
+					<MenuItem onClick={this.handleClose}>
+						<Link href="/facturasPagadas" to="/facturasPagadas">
+							Facturas Pagadas
+						</Link>
+					</MenuItem>
+					<MenuItem onClick={this.handleClose}>
+						<Link href="/facturasDevueltas" to="/facturasDevueltas">
+							Facturas Devueltas
+						</Link>
+					</MenuItem>
+				</Drawer>
+				<div>Respuesta de servicio: {this.state.text}</div>
+			</div>
+		);
+	}
 }
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired
+	title: PropTypes.string.isRequired
 };
 
 export default Header;
