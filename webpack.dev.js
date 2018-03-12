c360b7b90a3c66b9d1d03fc51f8383d77e27c147
Cesar <cesarag89@gmail.com>
2018-03-12 13:32:41 -0500

    directory changed

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,33 +10,33 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const serverPort = 3000;
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-    entry: [
-        "react-hot-loader/patch",
-        `webpack-dev-server/client?http://localhost:${serverPort}`,
-        "webpack/hot/only-dev-server",
-        path.resolve(__dirname, "src", "index.jsx")
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, "gestionpagosprevencion/dist"),
-        compress: true,
-        port: serverPort,
-        hot: true,
-        publicPath: "/gestionpagosprevencion/dist/",
-        historyApiFallback: true,
-        lazy: true,
-        filename: "[name].bundle.js",
-        open: true
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, "static", "index.html"),
-                to: path.resolve(__dirname, "gestionpagosprevencion/dist", "index.html"),
-                toType: "file"
-            }
-        ])
-    ]
+	mode: "development",
+	devtool: "inline-source-map",
+	entry: [
+		"react-hot-loader/patch",
+		`webpack-dev-server/client?http://localhost:${serverPort}`,
+		"webpack/hot/only-dev-server",
+		path.resolve(__dirname, "src", "index.jsx")
+	],
+	devServer: {
+		contentBase: path.join(__dirname, "dist/gestionpagosprevencion"),
+		compress: true,
+		port: serverPort,
+		hot: true,
+		publicPath: "/dist/gestionpagosprevencion/",
+		historyApiFallback: true,
+		lazy: true,
+		filename: "[name].bundle.js",
+		open: true
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new CopyWebpackPlugin([
+			{
+				from: path.resolve(__dirname, "static", "index.html"),
+				to: path.resolve(__dirname, "dist/gestionpagosprevencion", "index.html"),
+				toType: "file"
+			}
+		])
+	]
 });
