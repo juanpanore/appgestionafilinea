75805b394f16d981f01a2b556b77b392aa38ef4a
Cristian Bar Mt <cristian_barrientos23132@elpoli.edu.co>
2018-03-20 11:38:30 -0500

    fixed webpack deb config

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,35 +12,33 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const serverPort = 3000;
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-    entry: [
-        "react-hot-loader/patch",
-        `webpack-dev-server/client?http://localhost:${serverPort}`,
-        "webpack/hot/only-dev-server",
-        path.resolve(__dirname, "src", "index.jsx")
-    ],
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: serverPort,
-        hot: true,
-        historyApiFallback: true,
-        lazy: true,
-        open: true
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, "static", "index.html"),
-                to: path.resolve(__dirname, "dist", "index.html"),
-                toType: "file"
-            }
-        ])
-    ]
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: [
+    "react-hot-loader/patch",
+    `webpack-dev-server/client?http://localhost:${serverPort}`,
+    "webpack/hot/only-dev-server",
+    path.resolve(__dirname, "src", "index.jsx")
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    publicPath: "/public/",
+    historyApiFallback: true,
+    open: true,
+    port: serverPort,
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, "static", "index.html"),
+        to: path.resolve(__dirname, "dist", "index.html"),
+        toType: "file"
+      }
+    ])
+  ]
 });
