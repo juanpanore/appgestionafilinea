b7d74b900c6febd068fb2affa9386f4ca148b3f1
Jhoan Andrés Gutiérrez <jgutierrez@ias.com.co>
2018-03-21 11:19:15 -0500

    Change config webpack (test)

--- webpack.common.js
+++ webpack.common.js
@@ -4,56 +4,56 @@ const webpack = require("webpack");
 // const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-  context: __dirname,
-  output: {
-    filename: "[name].bundle.js",
-    publicPath: "/dist/",
-    path: path.resolve(__dirname, "dist")
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".json", ".css"]
-  },
-  stats: {
-    colors: true,
-    reasons: true,
-    chunks: true
-  },
+    context: __dirname,
+    output: {
+        filename: "[name].bundle.js",
+        publicPath: "/dist/",
+        path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json", ".css"],
+    },
+    stats: {
+        colors: true,
+        reasons: true,
+        chunks: true,
+    },
 
-  module: {
-    rules: [
-      {
-        test: /.jsx?$/,
-        enforce: "pre",
-        loader: "eslint-loader",
-        exclude: /node_modules/,
-        options: {
-          fix: true
-        }
-      },
-      {
-        test: /.jsx?$/,
-        use: "babel-loader"
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {}
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader",
-        include: /flexboxgrid/
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader"
-      }
-    ]
-  }
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                enforce: "pre",
+                loader: "eslint-loader",
+                exclude: /node_modules/,
+                options: {
+                    fix: true,
+                },
+            },
+            {
+                test: /.jsx?$/,
+                use: "babel-loader",
+            },
+            /* {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {},
+                    },
+                ],
+            }, */
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader",
+                include: /flexboxgrid/,
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+            },
+        ],
+    },
 };
 
 module.exports = config;
