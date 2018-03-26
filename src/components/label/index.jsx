d18aa96373bd125717c81b5aea3d0398c1ac09d5
Jhoan Andrés Gutiérrez <jgutierrez@ias.com.co>
2018-03-26 12:37:34 -0500

    update componente Label

--- src/components/label/index.jsx
+++ src/components/label/index.jsx
@@ -1,39 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
+import assign from "lodash/assign";
 
 const styles = {
     text: {
-        border: "1px solid #FF0"
+        paddingTop: 3,
+        paddingBottom: 3,
+        paddingLeft: 0,
+        paddingRight: 0,
+        marginTop: 10,
+        marginBottom: 10,
+        marginLeft: 0,
+        marginRight: 0,
+        fontSize: 16,
+        fontWeight: 400,
+        lineHeight: 1.5
+    },
+    title: {
+        paddingRight: 5,
+        fontWeight: 600
     }
 };
 
-/* class Label extends Component {
-    static propTypes = {
-        title: PropTypes.string,
-        text: PropTypes.string
-    };
-
-    static defaultProps = {
-        title: "",
-        text: ""
-    };
-
-    render() {
-        const { title, text } = this.props;
-        return (
-            <h3 style={styles.text}>
-                <strong>{title}</strong>
-                {text}
-            </h3>
-        );
-    }
-} */
-
 const Label = props => {
-    const { title, text } = props;
+    const { title, text, titleColor, textColor } = props;
     return (
-        <h3 style={styles.text}>
-            <strong>{title}</strong>
+        <h3 style={assign({}, styles.text, { color: textColor })}>
+            <strong style={assign({}, styles.title, { color: titleColor })}>
+                {title}
+            </strong>
             {text}
         </h3>
     );
@@ -41,12 +36,16 @@ const Label = props => {
 
 Label.propTypes = {
     title: PropTypes.string,
-    text: PropTypes.string
+    text: PropTypes.string,
+    titleColor: PropTypes.string,
+    textColor: PropTypes.string
 };
 
 Label.defaultProps = {
     title: "",
-    text: ""
+    text: "",
+    titleColor: "#000",
+    textColor: "#000"
 };
 
 export default Label;
